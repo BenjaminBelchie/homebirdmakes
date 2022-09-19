9c7b083eec071d91649dc844c4f7a83889d89456
BelchieBen <76218159+BelchieBen@users.noreply.github.com>
2022-09-19 20:23:58 +0100

    Added back test images and Shop Menu

--- components/InstagramFeed/index.tsx
+++ components/InstagramFeed/index.tsx
@@ -1,4 +1,4 @@
-import { ImageList } from "@mui/material";
+import { Divider, ImageList, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import ImageButton from "../ImageButton";
 
@@ -7,6 +7,8 @@ import ImageButton from "../ImageButton";
 export default function InstagramFeed(){
     return(
         <>
+            <Divider sx={{marginTop:2, marginBottom:2, width:"100%"}}><Typography variant="body2">Social Feed</Typography></Divider>
+            
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,12 @@ import {
   Stack,
   IconButton,
   ImageList,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+  InputLabel,
+  FormControl,
+  Menu,
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -13,6 +19,8 @@ import ImageButton from '../components/ImageButton';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import InstagramFeed from '../components/InstagramFeed';
+import React from 'react';
+import { useRouter } from 'next/router';
 
 export const getServerSideProps: GetServerSideProps = async(context) =>{
   // let etsyListings = null;
@@ -60,16 +68,32 @@ const testData = [
 ]
 
 const Home: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const shopMenuOpen = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleShopMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleShopCollectionClick = (e: React.MouseEvent) => {
+    router.push(`/collection/${e.currentTarget.dataset.value}`);
+  }
   
   return (
     <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
       {/* Top logo and icons */}
-      <Stack direction="row" justifyContent="space-between" sx={{width: '100%', margin:4}}>
-        <IconButton sx={{marginLeft:4}}>
+      <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{width: '100%', margin:4}}>
+        <IconButton sx={{marginLeft:4, height:"fit-content"}}>
           <SearchIcon/>
         </IconButton>
-        <Typography variant="h1">Homebird makes</Typography>
-        <IconButton sx={{marginRight:4}}>
+        <IconButton onClick={() => {router.push("/")}} sx={{padding:0}}>
+          <Box component="img" src="/images/homebirdmakes_logo.png"  sx={{height: "250px"}}/>
+        </IconButton>
+        
+        <IconButton sx={{marginRight:4, height:"fit-content"}}>
           <FavoriteIcon/>
         </IconButton>
       </Stack>
@@ -77,14 +101,37 @@ const Home: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideP
       {/* Nav Links */}
       <Stack direction="row" justifyContent="center">
           <Button variant="text">Home</Button>
-          <Button variant="text">Shop</Button>
+          <Button
+            id="basic-button"
+            aria-controls={shopMenuOpen ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={shopMenuOpen ? 'true' : undefined}
+            onClick={handleClick}
+          >Shop</Button>
+          <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={shopMenuOpen}
+              onClose={handleShopMenuClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem data-value="Pumpkins" onClick={handleShopCollectionClick}>Pumpkins</MenuItem>
+              <MenuItem data-value="Peony_Sage" onClick={handleShopCollectionClick}>Peony and Sage</MenuItem>
+              <MenuItem data-value="Cath_Kidson" onClick={handleShopCollectionClick}>Cath Kidson</MenuItem>
+              <MenuItem data-value="Mini_Hangers" onClick={handleShopCollectionClick}>Mini Hangers</MenuItem>
+              </Menu>
+          
           <Button variant="text">About</Button>
           <Button variant="text">Contact</Button>
       </Stack>
 
       <Typography variant="body1" sx={{marginTop:8}}>Collections</Typography>
       
-      <InstagramFeed />
+      <ImageList sx={{ width: "80%", height:800, margin:4 }} cols={3} rowHeight={250}>
+        {testData.map((item, index) => <ImageButton key={index} image={item.image} category={item.category}/>)}
+      </ImageList>
       
       
     </Box>
