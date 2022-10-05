015c0ab93786e824c336b4b7d924d945a21e231a
BelchieBen <76218159+BelchieBen@users.noreply.github.com>
2022-10-05 19:27:31 +0100

    Added new images to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,32 +32,32 @@ export const getServerSideProps: GetServerSideProps = async(context) =>{
 
 const testData = [
   {
-    image:"https://cdn.shopify.com/s/files/1/0014/1269/4073/collections/il_794xN.2560737347_gafl_360x.jpg?v=1613653847",
+    image:"/images/collections/pumkins.jpg",
     category: "Pumpkins",
     href:"https://www.etsy.com/uk/shop/homebirdmakes?section_id=29378110"
   },
   {
-    image:"https://cdn.shopify.com/s/files/1/0014/1269/4073/collections/peonyandsage1_360x.png?v=1557930739",
+    image:"/images/collections/baskets.jpeg",
     category: "Fabric Baskets",
     href:"https://www.etsy.com/uk/shop/homebirdmakes?section_id=22914008"
   },
   {
-    image:"https://cdn.shopify.com/s/files/1/0014/1269/4073/collections/cathk1_360x.png?v=1547837918",
+    image:"/images/collections/pouches.jpg",
     category: "Purses and Pouches",
     href:"https://www.etsy.com/uk/shop/homebirdmakes?section_id=21804003"
   },
   {
-    image:"https://cdn.shopify.com/s/files/1/0014/1269/4073/collections/il_794xN.1986567859_3oad_360x.jpg?v=1567435990",
+    image:"/images/collections/bookmarks.jpg",
     category: "Bookmarks",
     href:"https://www.etsy.com/uk/shop/homebirdmakes?section_id=24655291"
   },
   {
-    image:"https://cdn.shopify.com/s/files/1/0014/1269/4073/collections/il_794xN.1986567859_3oad_360x.jpg?v=1567435990",
+    image:"/images/collections/zipbag.jpg",
     category: "Large Zip Bags",
     href:"https://www.etsy.com/uk/shop/homebirdmakes?section_id=37258436"
   },
   {
-    image:"https://cdn.shopify.com/s/files/1/0014/1269/4073/collections/il_794xN.1986567859_3oad_360x.jpg?v=1567435990",
+    image:"/images/collections/bunting.jpg",
     category: "Garlands",
     href:"https://www.etsy.com/uk/shop/homebirdmakes?section_id=23547704"
   },
