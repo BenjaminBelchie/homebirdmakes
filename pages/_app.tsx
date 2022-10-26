958ec3709f693410047cf5fad06a9f160fc73f04
BelchieBen <76218159+BelchieBen@users.noreply.github.com>
2022-10-26 21:35:57 +0100

    Added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { createTheme, ThemeProvider } from '@mui/material'
 import { UserProvider } from '@auth0/nextjs-auth0';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import Script from 'next/script';
 
 const theme = createTheme({
   typography: {
@@ -35,6 +36,21 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+
+    <Script id="google-analytics" strategy="lazyOnload">
+      {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+                  `}
+    </Script>
     <QueryClientProvider client={queryClient}>
       <UserProvider>
         <ThemeProvider theme={theme}>
